a28300c802c764722b970ff8fce87b39112ea859
Nyi Nyi Thu <nyithu185@gmail.com>
2024-11-29 09:29:29 +0630

    create frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><Button>Click me</Button><h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}", flowbite.content(),],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite.plugin()],
+}
+
